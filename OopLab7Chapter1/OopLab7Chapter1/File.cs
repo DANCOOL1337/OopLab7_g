050275aaa123514efccd79a98e06d3229ce3f2b7
unknown <kozind311@gmail.com>
2026-05-15 02:33:36 +0500

    Test

---
+++ OopLab7Chapter1/OopLab7Chapter1/File.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.IO;
+
+
+namespace ConsoleApp1
+{
+    internal class File
+    {
+        private static Random _rnd;
+
+        static File()
+        {
+            _rnd = new Random();
+        }
+
+        public static void MakeFile1(uint n, string filename)
+        {
+            try
+            {
+                StreamWriter stream = new StreamWriter(filename);
+                for (uint i = 0; i < n; ++i)
+                {
+                    stream.WriteLine(_rnd.Next() % 1_000_000 - 500_000 + 1);
+                }
+                stream.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка в имени файла.");
+            }
+        }
+        public static void Task1(int k, string inputFile, string outputFile)
+        {
+            try
+            {
+                StreamReader input = new StreamReader(inputFile);
+                StreamWriter output = new StreamWriter(outputFile);
+
+                string line = "";
+                int n = 0;
+
+                while ((line = input.ReadLine()) != null)
+                {
+                    n = int.Parse(line);
+                    output.WriteLine(n / k);
+                }
+
+                input.Close();
+                output.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка в имени файла.");
+            }
+        }
+        public static void MakeFile2(uint n, uint k, string filename)
+        {
+            try
+            {
+                StreamWriter stream = new StreamWriter(filename);
+
+                for (int i = 0; i < n; ++i)
+                {
+                    for (int j = 0; j < k; ++j)
+                    {
+                        stream.Write(_rnd.Next() % 1_000_000 - 500_000 + 1);
+                        stream.Write(" ");
+                    }
+                    stream.WriteLine();
+                }
+
+                stream.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка в имени файла.");
+            }
+        }
+        public static int Task2(string filename)
+        {
+            try
+            {
+                string line;
+                StreamReader stream = new StreamReader(filename);
+                bool firstFlag = true;
+
+                int firstElem = 0;
+                int max = 0;
+                int elemInt = 0;
+
+                while ((line = stream.ReadLine()) != null)
+                {
+                    foreach (string elem in line.Split(' '))
+                    {
+                        if (elem == "")
+                        {
+                            continue;
+                        }
+                        elemInt = int.Parse(elem);
+                        if (firstFlag)
+                        {
+                            firstElem = elemInt;
+                            max = elemInt;
+                            firstFlag = false;
+                            continue;
+                        }
+                        if (elemInt > max)
+                        {
+                            max = elemInt;
+                        }
+                    }
+                }
+                return firstElem + max;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка в имени файла.");
+                return 0;
+            }
+        }
+        public static void MakeFile3(string filename)
+        {
+            try
+            {
+                StreamWriter stream = new StreamWriter(filename);
+                string alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+                int n = 33;
+                int k = 100;
+
+                for (int i = 0; i < n; ++i)
+                {
+                    for (int j = 0; j < k; ++j)
+                    {
+                        stream.Write(alph[_rnd.Next(33)]);
+                    }
+                    stream.WriteLine();
+                }
+                stream.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка в имени файла.");
+            }
+        }
+        public static void Task3(string inputFile, string outputFile)
+        {
+            try
+            {
+                StreamReader input = new StreamReader(inputFile);
+                StreamWriter output = new StreamWriter(outputFile);
+
+                string line;
+                while ((line = input.ReadLine()) != null)
+                {
+                    if (line[0] == 'б' || line[1] == 'б')
+                    {
+                        output.WriteLine(line);
+                    }
+                }
+
+                input.Close();
+                output.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка в имени файла.");
+            }
+        }
+        public static void MakeFile4(uint n, string filename)
+        {
+            try
+            {
+                FileStream fs = new FileStream(filename, FileMode.Create);
+                BinaryWriter stream = new BinaryWriter(fs);
+
+                Console.WriteLine("Исходный файл:");
+                int value = 0;
+
+                for (int i = 0; i < n; ++i)
+                {
+                    value = (int)(_rnd.Next() % 1_000_000 - 500_000 + 1);
+                    Console.WriteLine(value);
+                    stream.Write(value);
+                }
+                stream.Close();
+                fs.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка в имени файла.");
+            }
+        }
+        public static void Task4(string inputFile, string outputFile)
+        {
+            try
+            {
+                FileStream inputStream = new FileStream(inputFile, FileMode.Open);
+                FileStream outputStream = new FileStream(outputFile, FileMode.Create);
+
+                BinaryReader input = new BinaryReader(inputStream);
+                BinaryWriter output = new BinaryWriter(outputStream);
+
+                Console.WriteLine("\nРезультат работы программы:");
+
+                int value = 0;
+
+                while (inputStream.Position < inputStream.Length)
+                {
+                    value = input.ReadInt32();
+                    if (CheckNum(value))
+                    {
+                        Console.WriteLine(value);
+                        output.Write(value);
+                    }
+                }
+                input.Close();
+                output.Close();
+                inputStream.Close();
+                outputStream.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка в имени файла.");
+            }
+        }
+        private static bool CheckNum(int n)
+        {
+            if (n < 0)
+            {
+                n *= -1;
+            }
+            int a = n % 10;
+            int b = a;
+
+            while (n != 0)
+            {
+                b = n % 10;
+                n /= 10;
+            }
+            return a == b;
+        }
+    }
+}
---
+++ OopLab7Chapter2/OopLab7Chapter2/Structures.cs
@@ -0,0 +1,191 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace lab_2_2
+{
+    internal class Structures
+    {
+        private static Random _rnd;
+        
+        static Structures() 
+        {
+            _rnd = new Random();
+        }
+        public static List<int> MakeListInt(uint n)
+        {
+            List<int> res = new List<int>();
+
+            for (int i = 0; i < n; ++i)
+            {
+                res.Add(_rnd.Next() % 1_000 - 500 + 1);
+            }
+            return res;
+        }
+        public static List<int> Task6(List<int> list)
+        {
+            List<int> res = new List<int>(list.Count * 2);
+            foreach (int elem in list)
+            {
+                res.Add(elem);
+                res.Add(elem);
+            }
+            return res;
+        }
+        public static string ListIntToString(List<int> list)
+        {
+            string res = "";
+            for (int i = 0; i < list.Count - 1; ++i)
+            {
+                res += list[i].ToString() + ' ';
+            }
+            res += list[list.Count - 1];
+            return res;
+        }
+        public static LinkedList<int> MakeLinkedListInt(uint n)
+        {
+            LinkedList<int> res = new LinkedList<int>();
+            for (int i = 0; i < n; ++i)
+            {
+                res.AddLast(_rnd.Next() % 1_000 - 500 + 1);
+            }
+            return res;
+        }
+        public static string LinkedListIntToString(LinkedList<int> list)
+        {
+            if (list.Count == 0)
+            {
+                return "[]";
+            }
+            string res = list.First.Value.ToString();
+            for (LinkedListNode<int> elem = list.First.Next; elem != null; elem = elem.Next)
+            {
+                res += ' ' + elem.Value.ToString();
+            }
+            return res;
+        }
+        public static LinkedList<int> Task7(LinkedList<int> oldList)
+        {
+            
+            LinkedList<int> list = new LinkedList<int>(oldList);
+
+            
+            if (list.Count < 3) return list;
+
+            LinkedListNode<int> minNode = list.First;
+            LinkedListNode<int> maxNode = list.First;
+            LinkedListNode<int> curr = list.First;
+
+            
+            while (curr != null)
+            {
+                if (curr.Value < minNode.Value) minNode = curr;
+                if (curr.Value > maxNode.Value) maxNode = curr;
+                curr = curr.Next;
+            }
+
+            
+            Console.WriteLine("Min: " + minNode.Value);
+            Console.WriteLine("Max: " + maxNode.Value);
+
+            
+            LinkedListNode<int> firstBoundary = null;
+            LinkedListNode<int> secondBoundary = null;
+
+            curr = list.First;
+            while (curr != null)
+            {
+                if (curr == minNode || curr == maxNode)
+                {
+                    if (firstBoundary == null) firstBoundary = curr;
+                    else { secondBoundary = curr; break; }
+                }
+                curr = curr.Next;
+            }
+
+            
+            if (firstBoundary == secondBoundary || firstBoundary.Next == secondBoundary)
+            {
+                return list;
+            }
+
+            
+            LinkedListNode<int> left = firstBoundary.Next;
+            LinkedListNode<int> right = secondBoundary.Previous;
+
+            
+            while (left != null && right != null && left != right && left.Previous != right)
+            {
+                int temp = left.Value;
+                left.Value = right.Value;
+                right.Value = temp;
+
+                left = left.Next;
+                right = right.Previous;
+            }
+
+            return list;
+        }
+        public static KeyValuePair<int, SortedList<int, int>> ReadFile(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                return new KeyValuePair<int, SortedList<int, int>>(0, []);
+            }
+            StreamReader stream = new StreamReader(filename);
+            
+            int n = int.Parse(stream.ReadLine());
+            SortedList<int, int> list = new SortedList<int, int>();
+            string line = "";
+            int score = 0;
+
+            while ((line = stream.ReadLine()) != null)
+            {
+                score = int.Parse(line.Split(' ')[3]);
+                if (list.ContainsKey(score))
+                {
+                    ++list[score];
+                }
+                else
+                {
+                    list.Add(score, 1);
+                }
+            }
+            return new KeyValuePair<int, SortedList<int, int>>(n, list);
+        }
+        public static int Task10(KeyValuePair<int, SortedList<int, int>> pair)
+        {
+            int n = pair.Key;
+            SortedList<int, int> list = pair.Value;
+
+            int desired = (int)(n * 0.2);
+            int score = 0;
+            int lastScore = -1;
+            int count = 0;
+            int studentsCounted = 0;
+
+            for (int i = list.Count - 1; i >= 0; --i)
+            {
+                score = list.Keys[i];
+                count = list.Values[i];
+
+                if (studentsCounted + count == desired)
+                {
+                    return score;
+                }
+                else if (studentsCounted + count > desired)
+                {
+                    if (lastScore == -1)
+                    {
+                        return score;
+                    }
+                    else
+                    {
+                        return lastScore;
+                    }
+                }
+                studentsCounted += count;
+                lastScore = score;
+            }
+            return -1;
+        }
+    }
+}
